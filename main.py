1dae05267945c7bb8fe1276590fa1af1c373cea2
gnartiub <buiquynhtrang1234@gmail.com>
2024-01-30 15:11:41 +0100

    llm

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, jsonify
 import joblib
+from langchain.llms import Ollama
+
+ollama = Ollama(base_url='http://localhost:11434', model="llama2")
 
 app = Flask(__name__)
 regr = joblib.load("model.v1.bin")
@@ -20,13 +23,25 @@ def encode_prenom(prenom: str):
 @app.route("/predict")
 def predict():
     name = request.args.get("name")
-
-    if name:
-        sexe = regr.predict([encode_prenom(name)])
-        response = {
-            "name": name,
-            "gender": 'M' if sexe[0] == 0 else 'F',
-        }
-        return jsonify(response)
+    model = request.args.get("model")
+    if not model:
+        if name:
+            sexe = regr.predict([encode_prenom(name)])
+            response = {
+                "name": name,
+                "gender": 'M' if sexe[0] == 0 else 'F',
+            }
+            return jsonify(response)
+        else:
+            return jsonify({"error": "You need to pass name as GET parameter."})
     else:
-        return jsonify({"error": "You need to pass name as GET parameter."})
+        if name:
+            promt = "Classify the name {name} as Man's name or woman's name. If the name is man's, return 'H', if not return 'F'"
+            sexe = ollama(prompt)
+            response = {
+                "name": name,
+                "gender": sexe,
+            }
+            return jsonify(response)
+        else:
+            return jsonify({"error": "You need to pass name as GET parameter."})
