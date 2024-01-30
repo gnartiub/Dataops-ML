cbe7c4822b07954b065649a28f463eca53dfd4bd
gnartiub <buiquynhtrang1234@gmail.com>
2024-01-30 12:11:37 +0100

    change api and dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,26 +1,18 @@
-# Use the official Python image as a base image
-FROM python:3.10-slim
+FROM python:3.10
 
-# Set the working directory inside the container
-WORKDIR /app
-
-# Install curl
-RUN apt-get update && apt-get install -y curl
-
-# Install poetry
 RUN curl -sSL https://install.python-poetry.org | python3 -
 
-# Copy the poetry.lock and pyproject.toml files into the container
-COPY poetry.lock pyproject.toml ./
+RUN mkdir /app
+WORKDIR /app
 
-# Install dependencies using Poetry
-RUN poetry install
+ENV POETRY_VIRTUALENVS_CREATE=false
+ENV FLASK_PORT=5000
 
-# Copy the Flask application code into the container
+COPY poetry.lock poetry.lock
+COPY pyproject.toml pyproject.toml
+RUN $HOME/.local/bin/poetry install
+
 COPY . .
 
-# Expose port 5000 for Flask app
-EXPOSE 5000
-
-# Command to run the Flask application
-CMD ["poetry", "run","python", "main.py"]
+CMD ["bash", "-c", "flask --app api run --host 0.0.0.0 --port $FLASK_PORT"]
+# CMD ["flask", "--app", "api", "run", "--host", "0.0.0.0"]
--- main.py
+++ main.py
@@ -1,60 +1,32 @@
-import pickle
-import joblib
-from sklearn.linear_model import LogisticRegression
-
 from flask import Flask, request, jsonify
-# /Users/trangbui/Documents/ENSAI 3A/Dataops/MLOps
-import string
-import pandas as pd
+import joblib
+
+app = Flask(__name__)
+regr = joblib.load("model.v1.bin")
+
 
 def encode_prenom(prenom: str):
-    '''
-    
-    '''
-    alphabet = string.ascii_lowercase + 'é-'
+    """
+        This function encode a given name into a pd.Series.
+
+        For instance alain is encoded [1, 0, 0, 0, 0 ... 1, 0 ...].
+    """
+    alphabet = "abcdefghijklmnopqrstuvwxyzé-'"
     prenom = prenom.lower()
-    
-    return pd.Series([letter in prenom for letter in alphabet]).astype(int)
 
-class Predictor:
-    def __init__(self, model_path):
-        self.model_path = model_path
-        self.model = None
+    return [int(letter in prenom) for letter in alphabet]
 
-    def load_model(self):
-        # Load the logistic regression model from the specified path
-        with open(self.model_path, 'rb') as file:
-            self.model = joblib.load(file)
 
-    def predict(self, input_data):
-        if self.model is None:
-            self.load_model()
-        # Perform prediction using the loaded model
-        prediction = self.model.predict(input_data)
-        return prediction
+@app.route("/predict")
+def predict():
+    name = request.args.get("name")
 
-# Example usage:
-if __name__ == "__main__":
-    model_path = "model.v1.bin"
-    predictor = Predictor(model_path)
-    predictor.load_model()
-
-    app = Flask(__name__)
-
-    @app.route("/", methods=['GET'])
-    def root():
-        return {"message": "hello"}
-
-    @app.route('/predict', methods=['GET'])
-    def predict():
-        name = request.args.get("name")
-        if name:
-            sexe =  predictor.predict([encode_prenom(name)])
-            response = {
-                "name": name,
-                "response": 'H' if sexe[0] == 0 else 'F',
-            }
-            return jsonify(response)
-        else:
-            return jsonify({"error": "No prediction"})
-    app.run()
+    if name:
+        sexe = regr.predict([encode_prenom(name)])
+        response = {
+            "name": name,
+            "gender": 'M' if sexe[0] == 0 else 'F',
+        }
+        return jsonify(response)
+    else:
+        return jsonify({"error": "You need to pass name as GET parameter."})
